cf7b3bc6e14e144d656eded93c27e557ae11fade
your name here <your_email_here>
2019-07-08 14:03:40 -0400

    rps logic added

--- rpsrpg/src/App.js
+++ rpsrpg/src/App.js
@@ -16,8 +16,16 @@ export default class App extends Component {
       health:100,
       defense:15,
       attack:5
+    },
+    playerChoice:'',
+    compChoice:'',
+    board:{
+      tie:0,
+      won:0,
+      lost:0
     }
   }
+
   handleChange = e => {
     this.setState({
       [e.target.id]:e.target.value
@@ -31,16 +39,64 @@ export default class App extends Component {
       }
     })
   }
+  compChoiceMaker = () => {
+    this.setState({
+      compChoice:Math.floor(Math.random()*3)
+    })
+  }
+  submitBattle = e => {
+    e.preventDefault();
+    let { compChoice, playerChoice } = this.state;
+    this.compChoiceMaker()
+    if (playerChoice === compChoice){
+      console.log('tie')
+      this.setState({
+        board:{
+          tie:++this.state.board.tie
+        }
+      })
+    }
+    else if ((playerChoice - compChoice + 3) % 3 == 1){
+      console.log('won')
+      this.setState({
+        board:{
+          won:++this.state.board.won
+        }
+      })
+    }else{
+      console.log('lost')
+      this.setState({
+        board:{
+          lost:++this.state.board.lost
+        }
+      })
+    }
+    this.setState({
+      playerChoice:'',
+      compChoice:''
+    })
+
+
+  }
+  setChoice = e => {
+    this.setState({
+      [e.target.name]:e.target.id
+    })
+  }
+  //rock=0, paper=1, scissors=2
   render(){
-    let { input } = this.state;
-    let { hero } = this.state;
-    let { enemy } = this.state;
-    console.log(input)
+    let { input,hero,enemy,board } = this.state;
+    console.log(this.state)
     return (
       <>
       <div id='top'>
       <input id='input' onChange={this.handleChange} ></input>
       <button onClick={this.submitName}>Set Name</button>
+      </div>
+      <div id="scoreboard">
+      <h2>WON:{board.won}</h2>
+      <h2>TIE:{board.tie}</h2>
+      <h2>LOST:{board.lost}</h2>
       </div>
       <div className="App">
 
@@ -48,16 +104,28 @@ export default class App extends Component {
           <h3>{hero.health}</h3>
           <h1>{hero.name? hero.name:'Type your Name'}</h1>
           <img src={heroImg} alt='hero'></img>
+          <div>
+          <div>
+            <button id="0"  name="playerChoice" onClick={this.setChoice} >Rock</button>
+            <button id='1'  name="playerChoice"onClick={this.setChoice} >Paper</button>
+            <button id='2' name="playerChoice" onClick={this.setChoice} >Scissor</button>
+          </div>
+          </div>
         </div>
 
         <div id ='board'>
-        <button type='submit'> Battle </button>
+        <button onClick={this.submitBattle}> Battle </button>
         </div>
 
         <div id ='enemy'>
         <h3>{enemy.health}</h3>
         <h1>EvilPerson</h1>
         <img src={enemyImg} alt='enemy'></img>
+        <div>
+          <button id="0"  name="compChoice" onClick={this.setChoice} >Rock</button>
+          <button id='1'  name="compChoice"onClick={this.setChoice} >Paper</button>
+          <button id='2' name="compChoice" onClick={this.setChoice} >Scissor</button>
+        </div>
         </div>
 
       </div>
