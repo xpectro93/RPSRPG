e12bcc31089b22bfee3260c68649793efcb3bee8
xpectro93 <jonathanandrade@pursuit.org>
2019-07-09 16:27:06 -0400

    testing

--- rpsrpg/src/app2.js
+++ rpsrpg/src/app2.js
@@ -95,46 +95,7 @@ export default class App extends Component {
     console.log(this.state)
     return (
       <>
-      <div id='top'>
-      <input id='input' onChange={this.handleChange} ></input>
-      <button onClick={this.submitName}>Set Name</button>
-      </div>
-      <div id="scoreboard">
-      <h2>WON:{board.won}</h2>
-      <h2>TIE:{board.tie}</h2>
-      <h2>LOST:{board.lost}</h2>
-      </div>
-      <div className="App">
-
-        <div id='Hero'>
-          <h3>{hero.health}</h3>
-          <h1>{hero.name? hero.name:'Type your Name'}</h1>
-          <img src={heroImg} alt='hero'></img>
-          <div>
-          <div>
-            <button id="0"  name="playerChoice" onClick={this.setChoice} >Rock</button>
-            <button id='1'  name="playerChoice"onClick={this.setChoice} >Paper</button>
-            <button id='2' name="playerChoice" onClick={this.setChoice} >Scissor</button>
-          </div>
-          </div>
-        </div>
-
-        <div id ='board'>
-        <button onClick={this.submitBattle}> Battle </button>
-        </div>
-
-        <div id ='enemy'>
-        <h3>{enemy.health}</h3>
-        <h1>{enemy.name}</h1>
-        <img src={enemyImg} alt='enemy'></img>
-        <div>
-          <button id="0"  name="compChoice" onClick={this.setChoice} >Rock</button>
-          <button id='1'  name="compChoice"onClick={this.setChoice} >Paper</button>
-          <button id='2' name="compChoice" onClick={this.setChoice} >Scissor</button>
-        </div>
-        </div>
-
-      </div>
+      
       </>
     );
   }
